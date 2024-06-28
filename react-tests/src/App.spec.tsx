8e0aa4d3f53be6e9d0a208b3c54505f7804cbc1d
FelipeJhordan <felipejordan.alves@gmail.com>
2024-06-28 11:50:54 -0300

    feat(test-course): create the first test

---
+++ react-tests/src/App.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+
+import App from './App';
+
+describe("Testa o component App", () => {
+    test('Devem haver dois títulos na página', {}, async () => {
+        render(<App />)
+
+        const titles = await screen.findAllByRole("heading")
+
+        expect(titles).toHaveLength(2)
+    })
+
+    test("Deve ter um título na ESCRITO 'Hello World'", {}, async () => {
+        render(<App />)
+
+        const title = await screen.findByRole("heading", {
+            name: "Hello World!"
+        })
+
+        expect(title).toBeInTheDocument()
+    })
+});
